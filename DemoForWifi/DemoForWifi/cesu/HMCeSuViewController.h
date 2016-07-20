974b391cbe2a5bcdada3b91a52256d213008fc38
VolitationXiaoXing <752061676@qq.com>
2016-07-20 16:25:31 +0800

    测试数据

---
+++ DemoForWifi/DemoForWifi/cesu/HMCeSuViewController.h
@@ -0,0 +1,13 @@
+//
+//  HMCeSuViewController.h
+//  DemoForWifi
+//
+//  Created by Volitation小星 on 16/7/20.
+//  Copyright © 2016年 heima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMCeSuViewController : UIViewController
+
+@end
---
+++ DemoForWifi/DemoForWifi/cesu/HMCeSuViewController.m
@@ -0,0 +1,37 @@
+//
+//  HMCeSuViewController.m
+//  DemoForWifi
+//
+//  Created by Volitation小星 on 16/7/20.
+//  Copyright © 2016年 heima. All rights reserved.
+//
+
+#import "HMCeSuViewController.h"
+
+@interface HMCeSuViewController ()
+
+@end
+
+@implementation HMCeSuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
